dedaab07929b563f2b2aab9d4e482aaa3b74a225
Cody Milliron <codymilliron.dev@gmail.com>
2026-05-03 07:37:24 -0400

    create server connection

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,30 @@
+import amqp from "amqplib";
+
 async function main() {
   console.log("Starting Peril server...");
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("Started Peril server on port 5672...");
+  ["SIGINT", "SIGTERM"].forEach((signal) =>
+    process.on(signal, async () => {
+      try {
+        await conn.close();
+        console.log("RabbitMQ connection closed.");
+      } catch (err) {
+        console.error("Error closing RabbitMQ connection:", err);
+      } finally {
+        process.exit(0);
+      }
+    }),
+  );
 }
 
 main().catch((err) => {
   console.error("Fatal error:", err);
   process.exit(1);
 });
+
+function handleShutdown() {
+  console.log("Server is shutting down");
+  process.exit();
+}
