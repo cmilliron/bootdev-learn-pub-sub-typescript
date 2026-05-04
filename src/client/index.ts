1e403695d326c68e127324af70fc316bc3fd403f
Cody Milliron <codymilliron.dev@gmail.com>
2026-05-04 14:59:48 -0400

    add consumer channel and queue

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,37 @@
+import amqp from "amqplib";
+import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import { declareAndBind, SimpleQueueType } from "../internal/pubsub/consume.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+
 async function main() {
   console.log("Starting Peril client...");
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+
+  console.log("Peril game client connected to RabbitMQ!");
+
+  ["SIGINT", "SIGTERM"].forEach((signal) =>
+    process.on(signal, async () => {
+      try {
+        await conn.close();
+        console.log("RabbitMQ connection closed.");
+      } catch (err) {
+        console.error("Error closing RabbitMQ connection:", err);
+      } finally {
+        process.exit(0);
+      }
+    }),
+  );
+
+  // declare and bind queue
+  const userName = await clientWelcome();
+  const [channel, queue] = await declareAndBind(
+    conn,
+    ExchangePerilDirect,
+    `${PauseKey}.${userName}`,
+    PauseKey,
+    SimpleQueueType.Transient,
+  );
 }
 
 main().catch((err) => {
